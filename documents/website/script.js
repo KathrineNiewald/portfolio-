bde1f85bcab58c5e34d221af46e9cb358c678e0e
KathrineNiewald <112566559+KathrineNiewald@users.noreply.github.com>
2023-01-10 15:57:43 +0100

    burgermenu opdatering

---
+++ documents/website/script.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", sidenVises);
+function sidenVises(){
+    console.log("sidenVises")
+
+document.querySelector("#figur_container1").classList.add("blue");
+}
+
+
+
+
+ 
